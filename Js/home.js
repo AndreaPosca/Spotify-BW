dc6fd81c4d6643c192af7e3b8a274ebca9583561
AndreaPosca <andre_ibra8@hotmail.it>
2024-05-15 19:46:25 +0200

    modifiche momentanee

--- Js/home.js
+++ Js/home.js
@@ -7,3 +7,20 @@
 
 // Artist
 // https://striveschool-api.herokuapp.com/api/deezer/artist/75621062
+
+//Dichiarazionii variabili utili 
+let url = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    console.log(fetchAlbum());
+});
+
+const fetchAlbum = async() => {
+    await fetch (url)
+    .then((response) => {
+            return response.json();
+        })
+        
+}
+    

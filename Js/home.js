1055749cdd5ac22aa8d836f1cc6fb53a03d7cf67
Gianluca Chiaravalloti <91532537+nagcas@users.noreply.github.com>
2024-05-15 19:20:54 +0200

    creazione file home.js

---
+++ Js/home.js
@@ -0,0 +1,9 @@
+
+// Home page 
+// https://striveschool-api.herokuapp.com/api/deezer/search?q=queen
+
+// Album
+// https://striveschool-api.herokuapp.com/api/deezer/album/75621062
+
+// Artist
+// https://striveschool-api.herokuapp.com/api/deezer/artist/75621062

f668881ef94b76361d34b5ac0fd1752879df2f24
AndreaPosca <andre_ibra8@hotmail.it>
2024-05-15 21:05:00 +0200

    fine serata di mercoledì

--- Js/home.js
+++ Js/home.js
@@ -9,18 +9,30 @@
 // https://striveschool-api.herokuapp.com/api/deezer/artist/75621062
 
 //Dichiarazionii variabili utili 
-let url = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+let url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
 
 document.addEventListener("DOMContentLoaded", function () {
-    
-    console.log(fetchAlbum());
+    fetchAlbum()
 });
 
 const fetchAlbum = async() => {
     await fetch (url)
     .then((response) => {
             return response.json();
-        })
-        
+    })
+    .then ((musica) =>
+        popola(musica)
+    )
+    .catch ((error) => console.error(error))
 }
-    
+const card = document.getElementById('contenutoCard')
+function popola(datiCanzoni) {
+    for(i=0; i<7; i++) {
+  console.log(datiCanzoni[i]);
+}
+// const title = document.createElement('h4');
+// title.innerText = datiCanzoni.title; //da controllare come è scritto nell'oggeto il titolo generico
+
+// const img = document.createElement()
+//Funzione di popolamento HOME
+}

0fdfb20b3b0d612d39fca4f7bd3aae8cf15900b7
Gianluca Chiaravalloti <91532537+nagcas@users.noreply.github.com>
2024-05-16 15:56:47 +0200

    implementaza funzione di avvio canzone preview e pausa

--- Js/album.js
+++ Js/album.js
@@ -18,6 +18,7 @@ const url = "https://striveschool-api.herokuapp.com/api/deezer/album/75621062";
 
 const nomeArtista = document.getElementById("nomeArtista");
 
+const brano = document.getElementById("brano");
 
 document.addEventListener("DOMContentLoaded", function() {
    
@@ -42,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
     visualizzaTitolo(album);
     listaCanzoniAlbum(album);
+    previewBranoMP3();
   });
 });
 
@@ -49,9 +51,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * Visualizza il titolo nella hero del document html
  */
 function visualizzaTitolo(album) {
-  console.log(album);
+  //console.log(album);
   const visualizzaTitolo = document.getElementById("visualizzaTitolo");
-  console.log(visualizzaTitolo)
+  //console.log(visualizzaTitolo)
 
   visualizzaTitolo.innerHTML = `
     <div class="col-2">
@@ -73,7 +75,7 @@ function visualizzaTitolo(album) {
  * Visualizza la lista delle canzoni complete dell'album selezionato in forma tabellare
  */
 function listaCanzoniAlbum(album) {
-  console.log(album.tracks.data);
+  //console.log(album.tracks.data);
   const elencoCanzoni = document.getElementById("elencoCanzoni");
   
   let lista = 0;
@@ -82,17 +84,70 @@ function listaCanzoniAlbum(album) {
     const content = document.createElement("tr");
     console.table(canzone);
     content.innerHTML = `
-    <tr>
-      <th scope="row">${lista}</th>
-        <td>
-          <p>${canzone.title}</p>
-          <p>${canzone.album.title}</p>
-        </td>
-        <td>${canzone.rank}</td>
-        <td>${canzone.duration}</td>
-    </tr>
+      <tr>
+        <th scope="row">${lista}</th>
+          <td>
+            <p class="link-title" id="${canzone.id}">${canzone.title}</p>
+            <p>${canzone.album.title}</p>
+          </td>
+          <td>${canzone.rank}</td>
+          <td>${canzone.duration}</td>
+      </tr>
     `
-  elencoCanzoni.appendChild(content);
-  });
+    elencoCanzoni.appendChild(content);
 
+    const linkTitle = document.getElementById(`${canzone.id}`);
+    //const CanzoneInProduzione = document.getElementById("CanzoneInProduzione");
+    
+    const branoDaRiprodurre = document.getElementById("bra")
+
+    linkTitle.addEventListener(("click"), () => {
+
+      console.log(canzone);
+      console.log(linkTitle.textContent);
+      const titoloRiproduzione = document.getElementById("titoloRiproduzione");
+      const titoloRiproduzioneAlbum = document.getElementById("titoloRiproduzioneAlbum");
+      const viewAlbumRiproduzione = document.getElementById("viewAlbumRiproduzione");
+      const sfBiHeart = document.getElementById("sf-bi-heart");
+      
+      titoloRiproduzione.textContent = `${canzone.title}`;
+      titoloRiproduzioneAlbum.textContent = `${canzone.album.title}`;
+      
+      viewAlbumRiproduzione.innerHTML = `
+        <img src="${canzone.album.cover_small}" class="img-fluid" id="imageRiproduzione" alt="${canzone.album.title}">
+      `
+      sfBiHeart.classList.remove("d-none");
+
+      brano.textContent = `${canzone.preview}`;
+    })
+  });
+};
+
+/**
+ * Funzione di lettura brano musicale mp3 selezionato dalla lista delle canzoni
+*/
+let audio = new Audio();
+let isPlaying = false;
+
+function togglePlayPause() {
+
+  const controlPlay = document.getElementById("sf-btn-control-play");
+  if (!isPlaying) {
+    const mp3 = document.getElementById("brano").textContent;
+    // Imposta il percorso del file MP3
+    audio.src = mp3;
+    // Avvia la riproduzione del file MP3
+    audio.play();
+    // Cambia il testo del bottone in "Pausa"
+    controlPlay.innerHTML = `<i class="bi bi-pause"></i>`;
+    // document.querySelector('button').innerText = 'Pausa';
+    isPlaying = true;
+
+  } else {
+    // Interrompi la riproduzione del file MP3
+    audio.pause();
+    // Cambia il testo del bottone in "Play"
+    controlPlay.innerHTML = `<i class="bi bi-play"></i>`;
+    isPlaying = false;
+  }
 };
